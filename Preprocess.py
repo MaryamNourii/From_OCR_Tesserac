73222e0d21b1e09c36cddb36f9cc206aa2b79d67
MaryamNourii <maryam_nourii@yahoo.com>
2023-04-15 22:52:05 +0330

    Add preprocess methods

---
+++ Preprocess.py
@@ -0,0 +1,14 @@
+import cv2
+import os
+
+class preprocessing():
+    def __init__(self) -> None:
+        pass
+    def read_img(self, path):
+        pass
+    def enhancing_image(self, img):
+        pass
+    def detect_word(self, img):
+        pass
+    def save_words(self):
+        pass
