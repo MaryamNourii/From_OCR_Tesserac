0e378c3772051b225dc0400ec44321ef784c9755
MaryamNourii <maryam_nourii@yahoo.com>
2023-04-15 22:57:05 +0330

    Add tesseract_ocr methods

---
+++ OCR_Tesseract.py
@@ -0,0 +1,15 @@
+import os
+import re
+from PIL import Image
+from pytesseract import image_to_string
+import csv
+
+class OCR_Tesseract():
+    def __init__(self) -> None:
+        pass
+    def read_words(self):
+        pass
+    def ocr(self):
+        pass
+    def save_results(self):
+        pass
